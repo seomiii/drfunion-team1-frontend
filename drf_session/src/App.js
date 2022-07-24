e25a40d95fc3b0d4fb4809f331b439e0fd184910
seomiii <rlaaltj1765@gmail.com>
2022-07-24 18:48:30 +0900

    complete WritePost.js,Footer

--- drf_session/src/App.js
+++ drf_session/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
+import WritePost from './WritePost';
+import React from 'react';
+import { GlobalStyles } from './styles';
+import { MediaDiv } from './styledComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+
+      <MediaDiv>
+        <WritePost/>
+
+        
+      </MediaDiv>
+      <Footer/>
+      
     </div>
   );
 }
---
+++ drf_session/src/WritePost.js
@@ -0,0 +1,85 @@
+import React,{useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ContentsInput, 
+    PostTitle,
+    PostTitleDiv,
+    PostWriteDiv, 
+    TitleInput,
+    PostSubmit,
+    PostSubmitDiv } from './styledComponent';
+
+    
+const SubmitComponent = React.memo(({onSubmit}) => (
+    <PostSubmitDiv>
+      <PostSubmit onClick={onSubmit}>게시하기</PostSubmit>
+    </PostSubmitDiv>
+));
+
+//const navigate = useNavigate();
+
+const onSubmit=()=>{
+    console.log('submit');
+}
+
+// const onSubmit = () => {
+//     axios.post(`${apiUrl}/posts/`, {
+//       title: inputs.title,
+//       contents: inputs.contents,
+//       repls: [],
+//     }).then(() => {
+//       navigate('../');
+//     })
+//   }
+
+
+const WritePost = () => {
+    // useState 만들기
+    const [inputs, setInputs] = useState({
+        title: '',
+        contents: '',
+    });
+
+    // 2개를 동시에 관리하기 위한 객체
+    const { title, contents } = inputs;
+
+    const onChange=(e)=>{
+        const {value,name}=e.target;
+        setInputs({
+            ...inputs,
+            [name]:value,
+        });
+    };
+
+    console.log(inputs);
+
+    return (
+        <div>
+            <PostTitleDiv>
+                <PostTitle>고민 게시판</PostTitle>
+            </PostTitleDiv>
+
+            <PostWriteDiv>            
+                <TitleInput
+                name="title"
+                value={title}
+                type="text"
+                placeholder="글 제목"
+                onChange={onChange}
+                />
+        
+                <ContentsInput 
+                name="contents"
+                value={contents}
+                placeholder='고민을 입력해주세요.'
+                cols='30'rows="10"
+                onChange={onChange}
+                />
+            </PostWriteDiv>
+
+            <SubmitComponent onSubmit={onSubmit}></SubmitComponent>           
+            
+        </div>
+    );
+};
+
+export default WritePost;
