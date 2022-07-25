8b87489f2eb0c2d972940ce851e1e5a9b25d2978
Your Name <xi03hy@gmail.com>
2022-07-26 01:25:34 +0900

    라우트 변경22

--- drf/src/App.jsx
+++ drf/src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Borad from "./Borad";
 import items from "./mockup.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PostView from "./PostView";
 
 const App = () => {
   return (
-    <div>
-      <Borad items={items} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Borad items={items} />} />
+        <Route path="/postview/:id" element={<PostView />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
